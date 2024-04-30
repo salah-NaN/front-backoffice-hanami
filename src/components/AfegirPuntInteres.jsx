3f58e9f12c499d5fe588d81a4ed368c48cc9e2a7
Joel128 <joelplazadi@gmail.com>
2024-04-30 11:33:32 +0200

    pujada de fotos

--- src/components/AfegirPuntInteres.jsx
+++ src/components/AfegirPuntInteres.jsx
@@ -8,6 +8,9 @@ export const CrearPuntoInteres = () => {
   const { loguejat } = useContext(Contexto);
   const propietario_id = loguejat?.propietario_id;
 
+  const [imagen, setImagen] = useState(null); // Estado para almacenar la imagen seleccionada
+
+  const [puntoInteresId, setPuntoInteresId] = useState(null);
 
   const [currentPosition, setCurrentPosition] = useState({
     lat: "",
@@ -24,10 +27,15 @@ export const CrearPuntoInteres = () => {
     propietario_id: propietario_id,
   });
 
+  const subirImagen = (puntoInteresId) => {
+    console.log(puntoInteresId);
+  };
+
   useEffect(() => {
     setFormulario({
       ...formulario,
-      propietario_id: propietario_id,})
+      propietario_id: propietario_id,
+    });
   }, [propietario_id]);
 
   useEffect(() => {
@@ -64,149 +72,171 @@ export const CrearPuntoInteres = () => {
     setCurrentPosition({ lat, lon });
   };
 
-  const onSubmitForm = (e) => {
+  const onSubmitForm = async (e) => {
     e.preventDefault();
-    const credenciales = {
-      ...formulario,
-      latitud: currentPosition.lat,
-      longitud: currentPosition.lon,
-    };
-    const opciones = {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(credenciales),
-    };
-    fetch(`${API_URL}/puntos_interes`, opciones)
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data);
-        if (data.error) {
-          console.log(data.error);
-        } else {
-          console.log(data);
-        }
-      })
-      .catch((err) => console.log(err));
+    try {
+      // Crear un objeto FormData para enviar la imagen
+      const formData = new FormData();
+      formData.append("nombre", formulario.nombre);
+      formData.append("descripcion", formulario.descripcion);
+      formData.append("latitud", formulario.latitud);
+      formData.append("longitud", formulario.longitud);
+      formData.append("ubicacion", formulario.ubicacion);
+      formData.append("poblacion", formulario.poblacion);
+      formData.append("comarca", formulario.comarca);
+      formData.append("propietario_id", formulario.propietario_id);
+      formData.append("imagen", imagen); // Adjuntar la imagen al FormData
+
+      // Enviar los datos al servidor
+      const response = await fetch(`${API_URL}/puntos_interes`, {
+        method: "POST",
+        body: formData,
+      });
+      const data = await response.json();
+      if (data.error) {
+        console.error(data.error);
+      } else {
+        setPuntoInteresId(data.id);
+        console.log("Punto de interés creado:", data);
+        // Subir la imagen después de crear el punto de interés
+        subirImagen(data.id);
+      }
+    } catch (error) {
+      console.error("Error al enviar los datos:", error);
+    }
   };
 
   return (
     <div className="w-full h-full dark:bg-gray-800 dark:text-white">
-      <br/>
+      <br />
       {propietario_id ? (
         <div className="dark:bg-gray-800 dark:text-white w-full h-full">
           <div className="flex justify-center items-center h-full">
             <form onSubmit={onSubmitForm}>
               <h1 className="text-white">Añade un punto de interes.</h1>
               <div className="py-2">
-              <input
-                type="text"
-                name="nombre"
-                placeholder="nombre"
-                className="border border-black"
-                onInput={(e) =>
-                  setFormulario({ ...formulario, nombre: e.target.value })
-                }
-              ></input>
-            </div>
-            <div className="py-2">
-              <input
-                type="text"
-                name="descripcion"
-                placeholder="descipcion"
-                className="border border-black"
-                onInput={(e) =>
-                  setFormulario({
-                    ...formulario,
-                    descripcion: e.target.value,
-                  })
-                }
-              ></input>
-            </div>
-            <div className="py-2">
-              <input
-                type="text"
-                name="latitud"
-                placeholder="latidud"
-                value={currentPosition.lat}
-                className="border border-black"
-                onChange={(e) =>
-                  setFormulario({ ...formulario, latitud: e.target.value })
-                }
-              ></input>
-            </div>
-            <div className="">
-              <input
-                type="text"
-                name="logitud"
-                placeholder="longitud"
-                value={currentPosition.lon}
-                className="border border-black"
-                onInput={(e) =>
-                  setFormulario({ ...formulario, longitud: currentPosition.lon })
-                }
-              ></input>
-            </div>
-            <div className="py-2">
-              <input
-                type="text"
-                name="ubicacion"
-                placeholder="Calle"
-                className="border border-black"
-                onInput={(e) =>
-                  setFormulario({
-                    ...formulario,
-                    ubicacion: e.target.value,
-                  })
-                }
-              ></input>
-            </div>
-            <div className="py-2">
-              <input
-                type="text"
-                name="poblacion"
-                placeholder="poblacion"
-                className="border border-black"
-                onInput={(e) =>
-                  setFormulario({
-                    ...formulario,
-                    poblacion: e.target.value,
-                  })
-                }
-              ></input>
-            </div>
-            <div className="py-2">
-              <input
-                type="text"
-                name="comarca"
-                placeholder="comarca"
-                className="border border-black"
-                onInput={(e) =>
-                  setFormulario({ ...formulario, comarca: e.target.value })
-                }
-              ></input>
-            </div>
-            <div className="">
-              {currentPosition.lat && currentPosition.lon ? (
-                <MapSearchMyLocation
-                  currentPosition={currentPosition}
-                  updatePosition={updatePosition}
+                <input
+                  type="text"
+                  name="nombre"
+                  placeholder="nombre"
+                  className="border border-black"
+                  onInput={(e) =>
+                    setFormulario({ ...formulario, nombre: e.target.value })
+                  }
+                ></input>
+              </div>
+              <div className="py-2">
+                <input
+                  type="text"
+                  name="descripcion"
+                  placeholder="descipcion"
+                  className="border border-black"
+                  onInput={(e) =>
+                    setFormulario({
+                      ...formulario,
+                      descripcion: e.target.value,
+                    })
+                  }
+                ></input>
+              </div>
+              <div className="py-2">
+                <input
+                  type="text"
+                  name="latitud"
+                  placeholder="latidud"
+                  value={currentPosition.lat}
+                  className="border border-black"
+                  onChange={(e) =>
+                    setFormulario({ ...formulario, latitud: e.target.value })
+                  }
+                ></input>
+              </div>
+              <div className="">
+                <input
+                  type="text"
+                  name="logitud"
+                  placeholder="longitud"
+                  value={currentPosition.lon}
+                  className="border border-black"
+                  onInput={(e) =>
+                    setFormulario({
+                      ...formulario,
+                      longitud: currentPosition.lon,
+                    })
+                  }
+                ></input>
+              </div>
+              <div className="py-2">
+                <input
+                  type="text"
+                  name="ubicacion"
+                  placeholder="Calle"
+                  className="border border-black"
+                  onInput={(e) =>
+                    setFormulario({
+                      ...formulario,
+                      ubicacion: e.target.value,
+                    })
+                  }
+                ></input>
+              </div>
+              <div className="py-2">
+                <input
+                  type="text"
+                  name="poblacion"
+                  placeholder="poblacion"
+                  className="border border-black"
+                  onInput={(e) =>
+                    setFormulario({
+                      ...formulario,
+                      poblacion: e.target.value,
+                    })
+                  }
+                ></input>
+              </div>
+              <div className="py-2">
+                <input
+                  type="text"
+                  name="comarca"
+                  placeholder="comarca"
+                  className="border border-black"
+                  onInput={(e) =>
+                    setFormulario({ ...formulario, comarca: e.target.value })
+                  }
+                ></input>
+              </div>
+              <div className="">
+                {currentPosition.lat && currentPosition.lon ? (
+                  <MapSearchMyLocation
+                    currentPosition={currentPosition}
+                    updatePosition={updatePosition}
+                  />
+                ) : null}
+              </div>
+              <div className="py-2">
+                <input
+                  accept="image/*"
+                  type="file"
+                  name="imagen"
+                  className="border border-black"
+                  onChange={(e) => setImagen(e.target.files[0])}
                 />
-              ) : null}
-            </div>
-            <div className=" flex justify-center items-center py-6">
-              {console.log(formulario)}
-              <button className="py-2 px-4 bg-indigo-500 hover:bg-indigo-700 text-white font-semibold rounded-md" onClick={onSubmitForm}>Enviar</button>
-            </div>
+              </div>
+              <div className=" flex justify-center items-center py-6">
+                {console.log(formulario)}
+                <button
+                  className="py-2 px-4 bg-indigo-500 hover:bg-indigo-700 text-white font-semibold rounded-md"
+                  onClick={onSubmitForm}
+                >
+                  Enviar
+                </button>
+              </div>
             </form>
           </div>
         </div>
       ) : (
         <h1>Cargando data</h1>
       )}
-
     </div>
   );
 };
